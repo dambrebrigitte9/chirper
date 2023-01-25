ca6c3768c0d485f8c4149193fa246788e975ba0d
unknown <dambrebrigitte9>
2023-01-25 08:46:12 +0000

    creation d'une notification

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -15,9 +15,9 @@
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
-                    <x-nav-link :href="route('chirps.index')" :active="request()->routeIs('chirps.index')">
+                     <x-nav-link :href="route('chirps.index')" :active="request()->routeIs('chirps.index')">
                         {{ __('Chirps') }}
-                    </x-nav-link>
+                    </x-nav-link> 
                 </div>
             </div>
 
